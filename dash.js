9f4564d39395b731ee0fe5337cf54422a679ced5
Christian Madden <me@christianmadden.com>
2016-09-04 00:52:22 -0400

    Updates

--- dash.js
+++ dash.js
@@ -1,15 +1,16 @@
 
+require('dotenv').config()
 var request = require("request");
 var dash_button = require('node-dash-button');
 
-var dash = dash_button("a0:02:dc:73:d7:4d", "wlan0", 60000);
+var dash = dash_button(process.env.DASH_MAC_ADDRESS, process.env.NETWORK_INTERFACE, 60000);
 
 console.log("Starting up...");
 
 dash.on("detected", function()
 {
   console.log("Dash button pressed!");
-  request("http://christianmadden.ddns.net/api/routine/bedtime", function(error, response, body)
+  request(process.env.REQUEST_URL, function(error, response, body)
   {
     console.log(body);
   });
